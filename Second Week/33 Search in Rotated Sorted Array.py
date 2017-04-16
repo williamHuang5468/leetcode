20b120889c35078b10581d1014c8f33cf8f7c4f9
williamhuang <chobits5468@gmail.com>
2017-04-16 16:00:33 +0800

    [Update] finish the 33

---
+++ Second Week/33 Search in Rotated Sorted Array.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if target == nums[i]:
+                return i
+        else:
+            return -1
+
+''' Build in
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        try:
+            return nums.index(target)
+        except ValueError:
+            return -1
+'''
--- Second Week/README.md
+++ Second Week/README.md
@@ -1,5 +1,7 @@
 ## 0411 LeetCode
 
 - [x] #557
-- [ ] #412
+- [x] #412
 - [x] #344
+- [x] #33
+- [ ] [#535](https://leetcode.com/problems/encode-and-decode-tinyurl)
