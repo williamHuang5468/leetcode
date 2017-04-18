b4c73b0eb453a468ecf77c0632005f76426f5014
williamhuang <chobits5468@gmail.com>
2017-04-18 22:10:11 +0800

    [Update] local is right, but remote on leetcode is wrong.

--- Third Week/189 Rotate Array.py
+++ Third Week/189 Rotate Array.py
@@ -1,3 +1,4 @@
+'''
 class Solution(object):
     def rotate(self, nums, k):
         """
@@ -5,10 +6,53 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        pass
+        size = len(nums)
+        if size is not 0 and k is not 0:
+            offset = k % size
+            nums = nums[size-offset:] + nums[:size-offset]
+        return nums
+'''
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        size = len(nums)
+        if size is not 0 and k is not 0:
+            offset = k % size
+            right = [nums[i] for i in range(size-offset)]
+            left = [nums[i] for i in range(size-offset, size)]
+            return left + right
+        return nums
 
 '''
-n = k
-n > k
-n < k
+Probility situation
+1. nums_len = k
+2. nums_len > k
+3. nums_len < k
 '''
+s = Solution()
+assert s.rotate([], 0) == []
+assert s.rotate([2], 0) == [2]
+assert s.rotate([2], 1) == [2]
+assert s.rotate([2], 2) == [2]
+assert s.rotate([2], 3) == [2]
+assert s.rotate([1, 2], 1) == [2, 1]
+assert s.rotate([1, 2], 2) == [1, 2]
+assert s.rotate([1, 2, 3], 1) == [3, 1, 2]
+assert s.rotate([1, 2, 3], 2) == [2, 3, 1]
+assert s.rotate([1, 2, 3], 3) == [1, 2, 3]
+# n < k
+assert s.rotate([1, 2, 3], 4) == [3, 1, 2]
+assert s.rotate([1, 2, 3], 5) == [2, 3, 1]
+assert s.rotate([1, 2, 3], 6) == [1, 2, 3]
+assert s.rotate([1, 2, 3, 4], 1) == [4, 1, 2, 3]
+assert s.rotate([1, 2, 3, 4], 2) == [3, 4, 1, 2]
+assert s.rotate([1, 2, 3, 4], 3) == [2, 3, 4, 1]
+assert s.rotate([1, 2, 3, 4], 4) == [1, 2, 3, 4]
+assert s.rotate([1, 2, 3, 4], 5) == [4, 1, 2, 3]
+assert s.rotate([1, 2, 3, 4], 6) == [3, 4, 1, 2]
+assert s.rotate([1, 2, 3, 4], 7) == [2, 3, 4, 1]
+assert s.rotate([1, 2, 3, 4], 8) == [1, 2, 3, 4]
