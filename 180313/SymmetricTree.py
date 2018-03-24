d8896c8778b491a410d9864d4026dc35824f936d
williamhuang <chobits5468@gmail.com>
2018-03-24 17:50:35 +0800

    add brute force solution for SymmetricTree

--- 180313/SymmetricTree.py
+++ 180313/SymmetricTree.py
@@ -1,17 +1,28 @@
-class Node(object):
+class TreeNode(object):
     def __init__(self, value= None):
         self.left = None
         self.right = None
-        self.value = value
+        self.val = value
 
-class Tree(object):
-    def __init__(self, datas):
-        node = Node(1)
-        leftNode = Node(2)
-        rightNode = Node(2)
-        node.left = leftNode
-        node.right = rightNode
-        self.root = node
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        return self.visit(root.left, root.right)
+    
+    def visit(self, left , right):
+        if left is None and right is None:
+            return True
+        elif left is None or right is None:
+            return False
+        isEqual = left.val == right.val
+        isLeftEqual = self.visit(left.left, right.right)
+        isRightEqual = self.visit(left.right, right.left)
+        return isEqual and isLeftEqual and isRightEqual
 
 if __name__ == '__main__':
     pass
